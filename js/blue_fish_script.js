fd1c8086ce976385d921a34afe02448ec5ba2017
Mike Abbey <mihaiavram72@gmail.com>
2021-05-11 14:15:05 +0200

    RandomMovementFlip

--- js/blue_fish_script.js
+++ js/blue_fish_script.js
@@ -1,23 +1,33 @@
 $("#fish2Id").on("mouseover", function(e){
     $(this).stop(true);
-    var posY = getRandom(0, $(document).height() - 150);
     var posX = getRandom(0, $(document).width() - 150);
+    var posY = getRandom(0, $(document).height() - 150);
     if(e.pageX>posX) $(this).css({'transform':'scaleX(-1)'});
     else $(this).css({'transform':'scaleX(1)'});
-    $(this).animate({top: posY, left:posX},"slow");
-    randomMovement(this);
-})
+    $("#fish2Id").animate({top: posY,left: posX},1000,function(){
+        randomMovement(this);
+    });
+    
+});
+$(document).ready(function () {
+    
+    randomMovement("#fish2Id");
+});
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-$(document).ready(function () {
-    randomMovement("#fish2Id");
-});
+
 function randomMovement(idRef){
     var posY = getRandom(0, $(document).height() - 150);
     var posX = getRandom(0, $(document).width() - 150);
+    var elPosX=$(idRef).offset().left;
+    if(posX<elPosX) $(idRef).css({'transform':'scaleX(-1)'});
+    else  $(idRef).css({'transform':'scaleX(1)'});
     $(idRef).animate({top: posY,left: posX},4000, function(){
+        
         randomMovement(idRef);
     });
 };
+
+
 
