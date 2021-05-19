3a5e1544a109882cc4c733d9864b6c4c42339499
Emi-27 <emijos3@gmail.com>
2021-05-19 23:11:54 +0200

    oyster

--- js/orange_fish.js
+++ js/orange_fish.js
@@ -1,3 +1,4 @@
+//orange fish
 $(document).click(function (event) {
     $("#fish1Id").stop(true);
     $("#fish1Id").animate({left: event.pageX - 130, top: event.pageY - 130}, 1000);
@@ -27,3 +28,31 @@ function randomMoves(idRef, speed) {
         randomMoves(idRef, speed)
     });
 };
+//----------------------------------------------------------
+    //oyster
+    
+function bubbles_movements(idRef, speed) {
+   var x = getRandom(400, $(document).width()-300);
+    $(idRef).offset({width: 10, height: 10, top: 880, left: 960
+    });
+    $(idRef).animate({width: 10, height: 10, left: x, top: -100
+    }, speed, function () {
+        bubbles_movements(idRef, speed)
+    });
+}
+$(document).ready(function () {
+    bubbles_movements("#oysteBubble1Id", 2000).interval()
+});
+$(document).ready(function () {
+    bubbles_movements("#oysteBubble2Id", 3000).interval()
+});
+$(document).ready(function () {
+    bubbles_movements("#oysteBubble3Id", 2000).interval()
+});
+$(document).ready(function () {
+    bubbles_movements("#oysteBubble4Id", 5000).interval()
+});
+$(document).ready(function () {
+    bubbles_movements("#oysteBubble5Id", 4000).interval()
+});
+
