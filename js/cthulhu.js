84e3051b255d12fb002c03ca7ca2b36ffe204859
Mike Abbey <mihaiavram72@gmail.com>
2021-05-20 22:21:48 +0200

    Comments

--- js/cthulhu.js
+++ js/cthulhu.js
@@ -1,12 +1,18 @@
 
+
+//hides the image when the website is loaded
 $("#cthulhuId").hide();
+
+
 $(document).ready(function(){
     $(this).keydown(function(e){
         
         var key = (e.keyCode ? e.keyCode : e.which);
         e.preventDefault();
+        //in order to make cthulhu appear and do his thing you need to press "c"
         if(key==67) 
             cthulhuRises();
+        //then you can make him dissappear by pressing "d"
         else if(key==68){
             $("#cthulhuId").fadeOut();
             cthulhuAudio.pause();
@@ -20,6 +26,8 @@ $(document).ready(function(){
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+/* this function activates the sound associated with cthulhu, makes cthulhu visible by fading in 
+and then invokes the function that makes the element go in circles */
 function cthulhuRises(){
     var cthulhuAudio=document.getElementById("cthulhuAudio");
    
@@ -30,6 +38,9 @@ function cthulhuRises(){
     
 };
 
+
+/*this function moves the given element in a circular manner, based on its radius and x/y positions, 
+using trigonometric functions to simulate the positions at which it should move */
 var t = 0;
 
 function moveCthulhuInCircles() {
